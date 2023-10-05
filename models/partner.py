7a19015118ef2a3c702e7f8df42ffd46db10f73d
JoaquinRX <joquin@remixcom.com>
2023-10-05 02:27:14 -0300

    [FIX] seriable stock products in partner

--- models/partner.py
+++ models/partner.py
@@ -10,36 +10,39 @@ class PartnerInherit(models.Model):
     @api.depends('rx_product_line_ids', 'rx_product_line_ids.rx_qty')
     def _compute_product_line_ids(self):
         self.rx_product_line_ids = self.env['product.line']
-        products = {}
-        products_lots = {}
-        for task in self.env['project.task'].search([('rx_partner_id', '=', self.id), ('stage_id.name', '=', 'Finalizado')]):
-            inverter = 1
-            if (task.rx_order_type == 'returns'):
-                inverter = -1
-
+        products = []
+        assets_request_tasks = self.env['project.task'].search([('rx_partner_id', '=', self.id), ('stage_id.name', '=', 'Finalizado'), ('rx_order_type', '=', 'assets request')])
+        for task in assets_request_tasks:
             for line in task.rx_task_order_line_ids:
-                product = line.rx_stock_quant_id.product_id.id
-                if products.get(product):
-                    products[product] += line.rx_qty * inverter
+                product_id = line.rx_stock_quant_id.product_id.id
+                if not any(product_id == product.product_id for product in products):
+                    products.append(Line(product_id, line.rx_qty, line.rx_lot_ids.ids))
                 else:
-                    products[product] = line.rx_qty * inverter
+                    for product in products:
+                        if product.product_id == product_id:
+                            product.qty += line.rx_qty
+                            product.lot_ids += line.rx_lot_ids.ids
 
-                if products_lots.get(product):
-                    products_lots[product] += line.rx_lot_ids.ids
-                else:
-                    products_lots[product] = line.rx_lot_ids.ids
+        return_tasks = self.env['project.task'].search([('rx_partner_id', '=', self.id), ('stage_id.name', '=', 'Finalizado'), ('rx_order_type', '=', 'returns')])
+        for task in return_tasks:
+            for line in task.rx_task_order_line_ids:
+                product_id = line.rx_stock_quant_id.product_id.id
+                if any(product_id == product.product_id for product in products):
+                    for product in products:
+                        if product.product_id == product_id:
+                            product.qty -= line.rx_qty
+                            if line.rx_lot_ids.id in product.lot_ids:
+                                product.lot_ids.remove(line.rx_lot_ids.id)
 
-        for key, value in products.items():
-            product_id = self.env['product.product'].search([('id', '=', key)])
+        for product in products:
+            product_id = self.env['product.product'].search([('id', '=', product.product_id)])
             line = self.env['product.line'].create({
                 'rx_partner_id': self.id,
                 'rx_product_name': product_id.name,
-                'rx_product_id': key,
-                'rx_qty': value,
-                'rx_lot_ids': products_lots[key]
+                'rx_product_id': product.product_id,
+                'rx_qty': product.qty,
+                'rx_lot_ids': product.lot_ids
             })
-            if not product_id == line.rx_product_id:
-                self.rx_product_line_ids += line
 
 
 class ProductLine(models.Model):
@@ -51,3 +54,10 @@ class ProductLine(models.Model):
     rx_product_id = fields.Many2one('product.product', string='Product')
     rx_qty = fields.Integer('Quantity')
     rx_lot_ids = fields.Many2many('stock.production.lot')
+
+
+class Line:
+    def __init__(self, product_id, qty, lot_ids=None):
+        self.product_id = product_id
+        self.qty = qty
+        self.lot_ids = lot_ids
